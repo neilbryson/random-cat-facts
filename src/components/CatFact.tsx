46fa029e1e782aab4b07e1cda4f2adf80f3e294d
Neil Bryson Cargamento <neilbryson@users.noreply.github.com>
2021-05-01 20:54:02 +0800

    update style

--- src/components/CatFact.tsx
+++ src/components/CatFact.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { HTMLAttributes, ReactElement } from 'react';
 import styled from 'styled-components';
 
@@ -6,11 +7,26 @@ interface Props {
   fact: string;
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  border: 1px solid ${(props) => props.theme.app.body.TEXT_COLOR};
+  border-radius: 6px;
+  display: flex;
+  flex-flow: column nowrap;
+  padding: 12px;
+`;
 
-const Fact = styled.span``;
+const Fact = styled.span`
+  font-size: 42px;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 10px;
+  word-wrap: break-word;
+`;
 
-const Date = styled.span``;
+const Date = styled.span`
+  text-align: right;
+  font-size: 12px;
+`;
 
 export const CatFact = ({
   createdAt,
@@ -19,6 +35,6 @@ export const CatFact = ({
 }: Props & HTMLAttributes<HTMLDivElement>): ReactElement<HTMLDivElement> => (
   <Container {...other}>
     <Fact>{fact}</Fact>
-    <Date>{createdAt}</Date>
+    <Date>Submitted on {dayjs(createdAt).format('YYYY-MM-DD')}</Date>
   </Container>
 );
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 import { CatFact } from '../components/CatFact';
 import { useTypedSelector } from '../hooks/useTypedSelector';
-import { getRandomFact } from '../redux/modules/facts/actions';
+import { getRandomFact, selectNextFact, selectPrevFact } from '../redux/modules/facts/actions';
 
 const Container = styled.div`
   display: flex;
@@ -14,8 +14,27 @@ const Container = styled.div`
   padding: 30px 0;
 `;
 
-const RequestFactButton = styled.button`
-  flex-grow: 0;
+const NavigationButton = styled.button`
+  border: none;
+  border-radius: 6px;
+  height: 36px;
+`;
+
+const RequestFactButton = styled(NavigationButton)`
+  margin-bottom: 10px;
+`;
+
+const PrevNextContainer = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+
+  > button {
+    flex: 1 1 auto;
+
+    &:not(:last-of-type) {
+      margin-right: 10px;
+    }
+  }
 `;
 
 export const HomeView = (): ReactElement<HTMLDivElement> => {
@@ -40,10 +59,24 @@ export const HomeView = (): ReactElement<HTMLDivElement> => {
     dispatch(getRandomFact());
   }
 
+  function onClickPrevious(): void {
+    dispatch(selectPrevFact());
+  }
+
+  function onClickNext(): void {
+    dispatch(selectNextFact());
+  }
+
   return (
     <Container>
-      {renderFact()}
       <RequestFactButton onClick={onClickRequest}>Request random fact</RequestFactButton>
+      {factIds.length > 2 && (
+        <PrevNextContainer>
+          <NavigationButton onClick={onClickPrevious}>Select previous fact</NavigationButton>
+          <NavigationButton onClick={onClickNext}>Select next fact</NavigationButton>
+        </PrevNextContainer>
+      )}
+      {renderFact()}
     </Container>
   );
 };
